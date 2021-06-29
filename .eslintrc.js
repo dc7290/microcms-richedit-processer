86f23faa1c137dcfe7b6c454ce9b7944276e9a8a
dc7290 <dhkh.cba0927@icloud.com>
2021-06-29 21:06:43 +0900

    初期設定

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {},
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [],
+  rules: {},
+}
